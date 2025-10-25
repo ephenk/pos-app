905c95023bace7f011650a0623c7fe4f52f3259c
Stephen Hadisurja <stephen.hadisurja@gmail.com>
2025-10-25 10:45:54 +0700

    update package

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,17 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+        port: '',
+        pathname: '/wikipedia/**',
+      },
+    ],
+  },
+}
+
 export default nextConfig;
